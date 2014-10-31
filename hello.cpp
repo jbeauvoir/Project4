d0530ec6659d012f0ac9cc209ca0a3b7a117fdef
jbeauvoir <jbeauvoir2011@gmail.com>
2014-10-30 18:58:19 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 int main(){
-  # Default is "World"
-  name = ARGV.first || "World"
-  
-  
-  cout << "Hello, #{name}!" << endl;
+  using namespace std;
+
+int main(int argc, char* argv[]){
+  // Default is "World"                                                                                                   
+  //string name = ARGV.first || "World";                                                                                  
+  if (argc < 3){
+    cerr << "Usage: " << argv[0] << "--destination DESTINATION SOURCE" << endl;
+    return 1;
+  }
+   cout << "Hello, " << argv[10]  << endl;
 }
+
